676f5131c8c8e69d24f3af8353c5f38b42dca14a
Nanda Surya <nandasurya@Nandas-MacBook-Air.local>
2023-03-29 16:26:04 +0800

    update logso

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -6,12 +6,17 @@ const Footer = () => {
   return (
     <footer id="footer" className="bg-dark2 w-full font-Montserrat text-white">
       <div className="w-11/12 px-3 mx-auto py-20">
-        <div className={clsx("flex flex-col items-start justify-between", "md:flex-row md:items-center")}>
+        <div
+          className={clsx(
+            "flex flex-col items-start justify-between",
+            "md:flex-row md:items-center"
+          )}
+        >
           <div className="flex flex-col w-full">
             <img
-              src="https://ik.imagekit.io/lshkgi0dm/trixie/logo/Logo.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1670937740134"
+              src="https://ik.imagekit.io/lshkgi0dm/trixie_logo_1.svg?updatedAt=1680078171106"
               alt=""
-              className={clsx("w-10/12", "md:w-1/4")}
+              className={clsx("w-16", "md:w-24")}
             />
             <div className="w-1/4 bg-gray2 p-[1px] mt-7"></div>
             <p className={clsx("text-lg font-medium mt-5 w-1/6", "md:text-xl")}>
@@ -43,7 +48,8 @@ const Footer = () => {
               <li>
                 <a
                   href="https://www.instagram.com/trixiefootwear/"
-                  target="_blank" rel="noreferrer"
+                  target="_blank"
+                  rel="noreferrer"
                 >
                   Instagram
                 </a>
@@ -51,13 +57,18 @@ const Footer = () => {
               <li>
                 <a
                   href="https://twitter.com/Trixie_Footwear?t=kn3zE7oZIjxV2bABC7Y2vA&s=09"
-                  target="_blank" rel="noreferrer"
+                  target="_blank"
+                  rel="noreferrer"
                 >
                   Twitter
                 </a>
               </li>
               <li>
-                <a href="https://wa.me//085896131604" target="_blank" rel="noreferrer">
+                <a
+                  href="https://wa.me//085896131604"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   Whatsapp
                 </a>
               </li>

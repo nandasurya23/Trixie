61ea68ef88f15e09f1673139e9604e38067bb230
Nanda Surya <nandasurya@Nandas-MacBook-Air.local>
2023-04-01 13:32:38 +0800

    update footer and fixx minor

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -4,28 +4,37 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer id="footer" className="bg-dark2 w-full font-Montserrat text-white">
-      <div className="w-11/12 px-3 mx-auto py-20">
+    <footer
+      id="footer"
+      className={clsx("bg-dark2 w-full font-Montserrat text-white")}
+    >
+      <div className={clsx("container px-3 mx-auto py-20")}>
         <div
           className={clsx(
             "flex flex-col items-start justify-between",
             "md:flex-row md:items-center"
           )}
         >
-          <div className="flex flex-col w-full">
-            <img
-              src="https://ik.imagekit.io/lshkgi0dm/trixie_logo_putih.svg?updatedAt=1680079612664"
-              alt=""
-              className={clsx("w-16", "md:w-24")}
-            />
-            <div className="w-1/4 bg-gray2 p-[1px] mt-7"></div>
+          <div className={clsx("flex flex-col w-full")}>
+            <div className={clsx("flex items-center")}>
+              <img
+                src="https://ik.imagekit.io/lshkgi0dm/trixie_logo_putih.svg?updatedAt=1680079612664"
+                alt=""
+                className={clsx("w-16", "md:w-24")}
+              />
+              <h1 className={clsx("hidden md:block text-3xl mx-1.5 font-bold")}>
+                Trixie Footwear
+              </h1>
+            </div>
+
+            <div className={clsx("w-1/4 bg-gray2 p-[1px] mt-7")}></div>
             <p className={clsx("text-lg font-medium mt-5 w-1/6", "md:text-xl")}>
               Kapal, Badung 80351, Bali, Indonesia
             </p>
           </div>
-          <div className="flex flex-col w-1/4">
-            <h3 className="my-5 text-lg">Menu</h3>
-            <ul className="space-y-2">
+          <div className={clsx("flex flex-col w-1/4")}>
+            <h3 className={clsx("my-5 text-lg")}>Menu</h3>
+            <ul className={clsx("space-y-2")}>
               <li>
                 <Link href="/">Home</Link>
               </li>
@@ -42,9 +51,9 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className="flex flex-col w-1/4">
-            <h3 className="my-5 text-lg">Temukan Kami</h3>
-            <ul className="space-y-2">
+          <div className={clsx("flex flex-col w-1/4")}>
+            <h3 className={clsx("my-5 text-lg")}>Temukan Kami</h3>
+            <ul className={clsx("space-y-2")}>
               <li>
                 <a
                   href="https://www.instagram.com/trixiefootwear/"
@@ -75,8 +84,8 @@ const Footer = () => {
             </ul>
           </div>
         </div>
-        <div className="w-full bg-gray2 p-[1px] mt-10"></div>
-        <p className="text-center text-sm mt-5">
+        <div className={clsx("w-full bg-gray2 p-[1px] mt-10")}></div>
+        <p className={clsx("text-center text-sm mt-5")}>
           ©Trixie Footwear 2022. All rights reserved
         </p>
       </div>
